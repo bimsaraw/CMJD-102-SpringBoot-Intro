053c2742c11deccb2c4e51682f75f2e47efbd2bb
Bimsara Walallawita <bwalallawita@gmail.com>
2023-10-14 11:31:57 +0530

    Springboot Intro

---
+++ src/main/java/com/ijse/database/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.ijse.database.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ijse.database.entity.User;
+import com.ijse.database.service.UserService;
+
+@RestController
+public class UserController {
+    
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
+
+
---
+++ src/main/java/com/ijse/database/service/UserService.java
@@ -0,0 +1,14 @@
+package com.ijse.database.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ijse.database.entity.User;
+
+@Service
+public interface UserService {
+    List<User> getAllUsers();
+    User createUser(User user);
+    User getUserById(Long id);
+}
