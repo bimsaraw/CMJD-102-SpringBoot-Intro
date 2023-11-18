0cf7c4f129133ad4dafbdbe2e5490afff7480ab7
Bimsara Walallawita <bwalallawita@gmail.com>
2023-11-18 12:52:20 +0530

    Orders

--- src/main/java/com/ijse/database/controller/ProductController.java
+++ src/main/java/com/ijse/database/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.ijse.database.entity.Product;
 import com.ijse.database.service.ProductService;
 
 @RestController
+@CrossOrigin(origins = "*") //allowing cross origin to all
 public class ProductController {
     
     @Autowired
